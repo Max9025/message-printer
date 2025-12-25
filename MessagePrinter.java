100024aa940095eb57415c5d47f76f55cde4a845
Maksim Borminskyi <mborminskij@gmail.com>
2025-12-25 21:16:59 +0200

    Add author information

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -12,9 +12,14 @@ public class MessagePrinter {
         return "Thank you for using MessagePrinter!";
     }
 
+    public static String getAuthorMessage() {
+        return "Created by: Your Name";
+    }
+
     public static void main(String[] args) {
         System.out.println(getWelcomeMessage());
         System.out.println(getDateMessage());
+        System.out.println(getAuthorMessage())
         System.out.println("Learning remote repositories!");
         System.out.println(getGoodbyeMessage());
     }
