8496f5913721241155a73439f0be9b4a9ea9f651
Maksim Borminskyi <mborminskij@gmail.com>
2025-12-25 20:58:51 +0200

    Improve welcome message with greeting

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -1,7 +1,7 @@
 public class MessagePrinter {
 
     public static String getWelcomeMessage() {
-        return "Welcome to Git and GitHub!";
+        return "Hello! Welcome to Git and GitHub Tutorial!";
     }
 
     public static String getDateMessage() {
